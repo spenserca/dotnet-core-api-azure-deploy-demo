15393af1ebc9e0451fc378f64d686c00eb4fbe60
Spenser Andreassen <spenserca@gmail.com>
2019-05-07 17:52:36 -0500

    chore: remove local build step from deployment script

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -13,7 +13,7 @@ New-AzureRmResourceGroup `
 New-AzureRmResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -Mode Complete `
-    -TemplateFile '~\Repositories\Personal\GitHub\dotnet-core-api-azure-deploy-demo\azuredeploy.json' `
+    -TemplateFile '.\azuredeploy.json' `
     -OutVariable deployment `
     -Force `
     -Verbose
@@ -22,16 +22,6 @@ if ($deployment.ProvisioningState -ne 'Succeeded') {
     throw 'Deployment of Azure resources failed'
 }
 
-# Build the application for release
-# This should be done in your CI/CD pipeline
-cd C:\Users\Spen\Repositories\Personal\GitHub\dotnet-core-api-azure-deploy-demo\HelloWorld\HelloWorld
-
-dotnet build -c Release
-
-cd C:\Users\Spen\Repositories\Personal\GitHub\dotnet-core-api-azure-deploy-demo\HelloWorld\HelloWorld\bin\Release\netcoreapp2.0
-
-zip -r package.zip .
-
 # Get credentials to deploy the zip file
 $publishingCredentials = Invoke-AzureRmResourceAction `
     -ResourceGroupName $resourceGroupName `
@@ -39,16 +29,24 @@ $publishingCredentials = Invoke-AzureRmResourceAction `
     -ResourceName "$($resourceGroupName)/ondeck/publishingcredentials" `
     -Action list `
     -ApiVersion 2015-08-01 `
-    -Force
+    -Force `
+    -Verbose
 
 # Deploy the zip to the ondeck slot
 $username = $publishingCredentials.properties.publishingUserName
 $password = $publishingCredentials.properties.publishingPassword
-$filePath = 'C:\Users\Spen\Repositories\Personal\GitHub\dotnet-core-api-azure-deploy-demo\HelloWorld\HelloWorld\bin\Release\netcoreapp2.0\package.zip'
+$filePath = '.\package.zip'
 $apiUrl = "https://$($resourceGroupName)-ondeck.scm.azurewebsites.net/api/zipdeploy"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
 $userAgent = "powershell/1.0"
-Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $filePath -ContentType "multipart/form-data"
+Invoke-RestMethod `
+    -Uri $apiUrl `
+    -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} `
+    -UserAgent $userAgent `
+    -Method POST `
+    -InFile $filePath `
+    -ContentType "multipart/form-data" `
+    -Verbose
 
 # Run integration tests
 
