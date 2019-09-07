1687ae2421a2511cb94a0b25a56fa7d9727da862
Spenser Andreassen <spenserca@gmail.com>
2019-09-07 18:44:34 -0500

    chore: remove some deploy steps and configure them in az devops ui

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -12,7 +12,7 @@ New-AzureRmResourceGroup `
 
 # Create Azure resource group deployment
 # This will create the infrastructure described
-# in azuredeploy.json to host eh application
+# in azuredeploy.json to host the application
 New-AzureRmResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -Mode Complete `
@@ -24,32 +24,32 @@ New-AzureRmResourceGroupDeployment `
 if ($deployment.ProvisioningState -ne 'Succeeded') {
     throw 'Deployment of Azure resources failed'
 }
-
-# Get credentials to deploy the zip file
-$publishingCredentials = Invoke-AzureRmResourceAction `
-    -ResourceGroupName $resourceGroupName `
-    -ResourceType Microsoft.Web/sites/config `
-    -ResourceName "$($resourceGroupName)/publishingcredentials" `
-    -Action list `
-    -ApiVersion 2015-08-01 `
-    -Force `
-    -Verbose
-
-# Deploy the zip to the ondeck slot
-$username = $publishingCredentials.properties.publishingUserName
-$password = $publishingCredentials.properties.publishingPassword
-$filePath = "$($pathToDrop)/HelloWorld.zip"
-$apiUrl = "https://$($resourceGroupName).scm.azurewebsites.net/api/zipdeploy"
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
-$userAgent = "powershell/1.0"
-Invoke-RestMethod `
-    -Uri $apiUrl `
-    -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} `
-    -UserAgent $userAgent `
-    -Method POST `
-    -InFile $filePath `
-    -ContentType "multipart/form-data" `
-    -Verbose
+#
+## Get credentials to deploy the zip file
+#$publishingCredentials = Invoke-AzureRmResourceAction `
+#    -ResourceGroupName $resourceGroupName `
+#    -ResourceType Microsoft.Web/sites/config `
+#    -ResourceName "$($resourceGroupName)/publishingcredentials" `
+#    -Action list `
+#    -ApiVersion 2015-08-01 `
+#    -Force `
+#    -Verbose
+#
+## Deploy the zip to the ondeck slot
+#$username = $publishingCredentials.properties.publishingUserName
+#$password = $publishingCredentials.properties.publishingPassword
+#$filePath = "$($pathToDrop)/HelloWorld.zip"
+#$apiUrl = "https://$($resourceGroupName).scm.azurewebsites.net/api/zipdeploy"
+#$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+#$userAgent = "powershell/1.0"
+#Invoke-RestMethod `
+#    -Uri $apiUrl `
+#    -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} `
+#    -UserAgent $userAgent `
+#    -Method POST `
+#    -InFile $filePath `
+#    -ContentType "multipart/form-data" `
+#    -Verbose
 
 # Run integration tests
 
